42548d24e3e2495c83bdc81b59f03ead73887ff3
shih1999 <shihchen1999@gmail.com>
2024-10-06 18:26:32 +0800

    event module, modify layout

--- src/utils/APIRequests/apis/event.js
+++ src/utils/APIRequests/apis/event.js
@@ -1,6 +1,6 @@
 import request from '@/utils/APIRequests/requests.js'
 
-export const fetchCADApi = (date_min, date_max, dist_max ) => {
+export const fetchCadApi = (date_min, date_max, dist_max ) => {
     return request({
         url: `cad.api?date-min=${date_min}&date-max=${date_max}&dist-max=${dist_max}`
     })
